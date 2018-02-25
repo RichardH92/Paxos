f4ff27d8770a20a441981b4c5dc529271b47f29d
Richard Hart <rhart@medallia.com>
2018-02-25 12:32:12 -0500

    Started implementing learner type.

--- learner.go
+++ learner.go
@@ -1,1 +1,29 @@
 package main
+
+import "errors"
+
+type Learner struct {
+	NodeData
+}
+
+func (learner Learner) HandleMessage(msg Message) (retMsg Message, err error) {
+	switch msg.Type {
+
+	case accepted:
+		return learner.handleAccepted(msg)
+
+	default:
+		return msg, errors.New("Invalid message type")
+	}
+}
+
+func (learner Learner) handleAccepted(msg Message) (retMsg Message, err error) {
+
+}
+
+func (learner Learner) AddNode(node NodeData) (err error) {
+	switch node.Type {
+	default:
+		return errors.New("Invalid node type")
+	}
+}
