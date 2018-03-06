9725e7cec637a5322a820baa89c523c912a2ee84
Richard Hart <rhart@medallia.com>
2018-03-05 20:38:41 -0500

    Changed propose to use pointers.

--- proposor.go
+++ proposor.go
@@ -6,36 +6,35 @@ type Proposor struct {
 	NodeData
 }
 
-func (proposor Proposor) HandleMessage(msg Message, nodeData NodeData) (err error) {
+func (proposor *Proposor) HandleMessage(msg Message, nodeData *NodeData) (err error) {
 	switch msg.Type {
-	case accepted:
-		return handleAcceptedMessage(msg, nodeData)
+	case promise:
+		return proposor.handlePromiseMessage(msg, nodeData)
 
 	case accept:
-		//TODO: implement here
-		return nil
+		return proposor.handleAcceptMessage(msg, nodeData)
 
 	default:
 		return errors.New("Invalid message type")
 	}
 }
 
-func handleAcceptedMessage(msg Message, nodeData NodeData) (err error) {
+func (proposor *Proposor) handlePromiseMessage(msg Message, nodeData *NodeData) (err error) {
 	return nil
 }
 
-func handleAcceptMessage(msg Message, nodeData NodeData) (err error) {
+func (proposor *Proposor) handleAcceptMessage(msg Message, nodeData *NodeData) (err error) {
 	return nil
 }
 
-func (proposor Proposor) AddNode(node NodeData) (err error) {
+func (proposor *Proposor) AddNode(node *NodeData) (err error) {
 	switch node.Type {
 	default:
 		return errors.New("Invalid node type")
 	}
 }
 
-func (proposor Proposor) RemoveNode(node NodeData) (err error) {
+func (proposor *Proposor) RemoveNode(node *NodeData) (err error) {
 	switch node.Type {
 	default:
 		return errors.New("Invalid node type")
